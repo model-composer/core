74e3ad5857b9fc0690de19a0ee5d79ab00f33eb7
Domenico Giambra <zorinik@gmail.com>
2022-05-08 14:53:38 +0200

    First commit

---
+++ src/Core.php
@@ -0,0 +1,34 @@
+<?php namespace Model\Core;
+
+use Composer\InstalledVersions;
+use Model\Config\Config;
+
+class Core
+{
+	public static function init(): void
+	{
+		Config::loadEnv();
+
+		$config = self::getConfig();
+		if (!defined('APP_NAME'))
+			define('APP_NAME', $config['name']);
+		if (!defined('PATH'))
+			define('PATH', $config['path']);
+	}
+
+	public static function cleanUp(): void
+	{
+		if (InstalledVersions::isInstalled('model/cache'))
+			\Model\Cache\Cache::invalidate();
+	}
+
+	public static function getConfig(): array
+	{
+		return Config::get('core', function () {
+			return [
+				'name' => defined('APP_NAME') ? APP_NAME : '',
+				'path' => defined('PATH') ? PATH : '/',
+			];
+		});
+	}
+}
